846ca1a0eb017445a4a0c362d95f7207c8c6a4ef
dgamarra <dgamarra@continental.edu.pe>
2025-09-26 20:53:43 -0500

    Tercer commit

--- backend/__tests__/users.e2e.test.js
+++ backend/__tests__/users.e2e.test.js
@@ -31,7 +31,7 @@ describe("Pruebas E2E de usuarios", () => {
   test("GET /api/users devuelve usuarios", async () => {
     await User.create({ name: "María" });
     const res = await request(app).get("/api/users");
-    expect(res.status).toBe(201);
+    expect(res.status).toBe(200);
     expect(res.body.length).toBe(1);
     expect(res.body[0].name).toBe("María");
   });
